9e95b66e8ae6b5be5202dcee8f6ae01e24b6d7a3
Justin O'Connor <justinoconnor@justins-air.lan>
2023-06-12 21:11:03 -0400

    chore: make tags lowercase

--- examples/locals.tf
+++ examples/locals.tf
@@ -3,11 +3,11 @@ locals {
   tags = merge(
     var.tags,
     {
-      "GITHUB SHA"        = var.github_sha
-      "GITHUB REF NAME"   = var.github_ref_name
-      "GITHUB ACTOR"      = var.github_actor
-      "GITHUB REPOSITORY" = var.github_repository
-      "ENVIRONMENT"       = var.environment
+      "github sha"        = var.github_sha
+      "github ref name"   = var.github_ref_name
+      "github actor"      = var.github_actor
+      "github repository" = var.github_repository
+      "environment"       = var.environment
     }
   )
 }
